680ec97d5d6b615a57785fb5412b0cd71f3fce16
astraliq <astraliq457@gmail.com>
2019-12-07 16:45:09 +0300

    change container css

--- views/activity/browsing.php
+++ views/activity/browsing.php
@@ -2,12 +2,12 @@
 
 ?>
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-14">
         <h1 class="text-center">Календарь событий</h1>
         <div class="col-md-3" id="calendar_block">
 
         </div>
-        <div class="col-md-12">
+        <div class="col-md-14">
 
             <?= \yii\grid\GridView::widget([
                     'dataProvider' => $provider,
